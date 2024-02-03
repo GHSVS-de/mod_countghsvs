a47950930ab1c63b0108fb1d01cbae994b072486
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2024-02-03 14:11:33 +0100

    Layout review. Add backgroundimage etc.

--- src/tmpl/default.php
+++ src/tmpl/default.php
@@ -1,6 +1,9 @@
 <?php
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\HTML\HTMLHelper;
+
 /*
 From Dispatcher:
 $module   Object,
@@ -20,19 +23,26 @@ if (empty($items))
 {
 	return '';
 }
+
+if ($params->get('backgroundimage', ''))
+{
+	$wa->addInlineStyle(
+'.zaehlers.' . $modId . '{background-image: url("' . Uri::root(true) . '/' . HTMLHelper::_('cleanImageURL', $params->get('backgroundimage'))->url . '");}
+', ['name' => $modId]);
+}
 ?>
-<div class="container" id="<?php echo $modId; ?>">
-	<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-		<?php foreach($items as $key => $item)
-		{
-			$classCollector[$item->class] = 1;
-		?>
-			<div class="card">
-				<div class="card-body">
-					<p class="<?php echo $item->class;?>"><?php echo $item->end;?></p>
-					<p class="card-text"><?php echo $item->text;?></p>
-				</div>
-			</div>
+<div id="<?php echo $modId; ?>" class="container-fluid mod_countghsvs zaehlers <?php echo $modId; ?>">
+	<?php if ($module->showtitle)
+	{
+		echo '<h1 class=moduleHeadline>' . $module->title . '</h1>';
+	}?>
+	<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+		<?php foreach ($items as $key => $item)
+		{ ?>
+					<div class="card-bodyCycle mx-auto d-flex align-items-center flex-wrap text-center">
+						<div class="<?php echo $item->class;?> zaehlerNumber w-100 align-self-end"><?php echo $item->start;?></div>
+						<div class="card-text donthyphenate w-100 align-self-start pt-3"><?php echo nl2br($item->text);?></div>
+					</div>
 		<?php
 		} ?>
 	</div>
